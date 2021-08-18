41b1ba19dd3eeef1025477549a1873f48bfde518
joluoch <jakindaodhiambo@gmail.com>
2021-08-18 12:30:46 +0000

    getting data using pdal pipeline

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -1,31 +1,59 @@
 import pdal 
 import json 
+import sys
 
 
-PUBLIC_DATA_PATH = 'https://s3-us-west-2.amazonaws.com/usgs-lidar-public/'
 
-Region = 'IA_FullState/'
+def get_region_bound(Region :str , bound : str ,):
+    ''' This function is going to promt a user to enter the desired region and bound'''
+    PUBLIC_DATA_PATH = "https://s3-us-west-2.amazonaws.com/usgs-lidar-public/"
+    pipeline_path = '../scripts/get_data.json'
+    
+    
+    print ('===================REQUESTING REGION  =====================')
+    Region = (input("Enter the Region : ")) #AI_FULLSTATE
+    print (Region)
+    
+    
+    print ('===================REQUESTING  BOUNDS =====================')
+    bound = (input("Enter the Bounds [MINX, MINY, MAXX, MAXY] : ")) #bound = "([-11669524.7, -11666600.8], [4776607.3, 4778714.4])"
+    print(bound)
+    
+    print ('===================REQUEST PATH =====================')
+    access_path = PUBLIC_DATA_PATH + Region + '/ept.json'
+    print(access_path)
+    
+    print ('===================OUPUT LAZ PATH =====================')
+    output_filename_laz = "../laz/"+Region+".laz"
+    print(output_filename_laz)
+    
+    print ('===================OUPUT TIF PATH =====================')
+    output_filename_tif = "../tif/"+Region+".tif"
+    print(output_filename_tif)
+    
+    
+    
+    return Region, bound, access_path, output_filename_laz, output_filename_tif, pipeline_path
 
-bound = "([-10425171.940, -10423171.940], [5164494.710, 5166494.710])"
+#Region = 'USGS_LPC_CO_SoPlatteRiver_Lot5_2013_LAS_2015/'
+#bound = "([-11669524.7, -11666600.8], [4776607.3, 4778714.4])"
+#access_path = PUBLIC_DATA_PATH + Region + 'ept.json'
+#output_filename_laz = '../laz/USGS_LPC_CO_SoPlatteRiver_Lot5_2013_LAS_2015.laz'
+#ouput_filename_tif = '../tif/USGS_LPC_CO_SoPlatteRiver_Lot5_2013_LAS_2015.tif'
+#pipeline_path = '../scripts/get_data.json'
 
-access_path = PUBLIC_DATA_PATH + Region + 'ept.json'
-
-output_filename_laz = '../laz/iowa.laz'
-ouput_filename_tif = '../tif/iowa.tif'
-pipeline_path = '../scripts/get_data.json'
 
 
-def get_raster_terrain(bounds=bound , region=Region, public_access_path = access_path,output_filename_laz=output_filename_laz,
-                       ouput_filename_tif = ouput_filename_tif,pipeline_path =pipeline_path ):
+def get_raster_terrain(region ,bounds , access_path , output_filename_laz,ouput_filename_tif,pipeline_path ):
     
     with open(pipeline_path) as json_file:
         the_json = json.load(json_file)
         
         
     the_json['pipeline'][0]['bounds']=bounds
-    the_json['pipeline'][0]['filename']=public_access_path
-    the_json['pipeline'][6]['filename']=output_filename_laz
-    the_json['pipeline'][7]['filename']=ouput_filename_tif
+    the_json['pipeline'][0]['filename']=access_path
+    the_json['pipeline'][5]['filename']=output_filename_laz
+    the_json['pipeline'][6]['filename']=ouput_filename_tif
     
     pipeline = pdal.Pipeline(json.dumps(the_json))
     
@@ -37,10 +65,13 @@ def get_raster_terrain(bounds=bound , region=Region, public_access_path = access
     except RuntimeError as e :
         print(e)
         pass
-        
+      
          
 if (__name__== '__main__'):
-    get_raster_terrain()
+    region=sys.argv[0]
+    bound=sys.argv[1]
+    Region,bound,access_path,output_filename_laz,output_filename_tif,pipeline_path=get_region_bound(region,bound)
+    get_raster_terrain(Region,bound,access_path,output_filename_laz,output_filename_tif,pipeline_path)
     
     
 
